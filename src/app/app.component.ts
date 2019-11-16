e6938731043d0a4947abe2a9191da86b48036815
pcu4troblo <juanpablor56@gmail.com>
2019-11-15 20:59:01 -0500

    updated

---
+++ src/app/app.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { NumbersService } from './services/numbers.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  size: any;
+  number: string;
+  numbers: Array<any> = [];
+  failed: boolean = false;
+
+  constructor(private service: NumbersService) { }
+
+  print() {
+    if(this.size>10 || this.size<0){
+      this.failed = true;
+       return
+    }
+    if (this.number == '0' && this.size == 0) {
+      console.clear();
+    } else {
+      this.numbers = this.number.split("");
+      this.numbers.forEach(num => {
+        switch (num) {
+          case "1":
+            this.service.makeOne(this.size);
+            break;
+          case "2":
+            this.service.makeTwo(this.size);
+            break;
+          case "3":
+            this.service.makeThree(this.size);
+            break;
+          case "4":
+            this.service.makeFour(this.size);
+            break;
+          case "5":
+            this.service.makeFive(this.size);
+            break;
+          case "6":
+            this.service.makeSix(this.size);
+            break;
+          case "7":
+            this.service.makeSeven(this.size);
+            break;
+          case "8":
+            this.service.makeEight(this.size);
+            break;
+          case "9":
+            this.service.makeNine(this.size);
+            break;
+          case "0":
+            this.service.makeZero(this.size);
+            break;
+        }
+      })
+    }
+  }
+
+}
+
+
+
+
---
+++ src/app/services/numbers.service.ts
@@ -0,0 +1,356 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NumbersService {
+
+  constructor() { }
+
+  makeOne(size: number): string {
+    let elements: Array<any> = ["\n", " |", "\n", " |"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeTwo(size: number) {
+    let elements: Array<any> = [" -", " |", " -", "|", "\n", " -"];
+    if (size > 1) {
+      elements = [" -", " |", " -", "|", " -"];
+    }
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeThree(size: number) {
+    let elements: Array<any> = [" -", " |", " -", " |", " -"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeFour(size: number) {
+    let elements: Array<any> = ["\n", "| |", " -", " |"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeFive(size: number) {
+    let elements: Array<any> = [" -", "|", "\n", " -", " |", " -"];
+    if (size > 1) {
+      elements = [" -", "|", " -", " |", " -"];
+    }
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+    return number;
+  }
+
+  makeSix(size: number) {
+    let elements: Array<any> = [" -", "|", "\n", " -", "| |", " -"];
+    if (size > 1) {
+      elements = [" -", "|", " -", "| |", " -"];
+    }
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeSeven(size: number) {
+    let elements: Array<any> = [" -", " |", "\n", " |", "\n"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeEight(size: number) {
+    let elements: Array<any> = [" -", "| |", " -", "| |", " -"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeNine(size: number) {
+    let elements: Array<any> = [" -", "| |", " -", " |", "\n"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+  makeZero(size: number) {
+    let elements: Array<any> = [" -", "| |", "\n", "| |", " -"];
+    let number: string = "";
+    elements.forEach(char => {
+      switch (char) {
+        case "\n":
+          number = number + this.assignSpace(size);
+          break;
+        case "|":
+          number = number + this.assignLeftBar(size);
+          break;
+        case " |":
+          number = number + this.assignRightBar(size);
+          break;
+        case " -":
+          number = number + this.assignScript(size);
+          break;
+        case "| |":
+          number = number + this.assignBothBars(size);
+          break;
+      }
+    })
+    console.log(number);
+
+    return number;
+  }
+
+
+  assignScript(size: number): string {
+    let arr: Array<any> = [];
+    let character = " -\n";
+    arr = character.split("");
+    for (let i = 1; i < size; i++) {
+      arr.splice(1, 0, "-")
+    }
+    character = arr.join("");
+    return character;
+  }
+
+  assignRightBar(size: number): string {
+    let arr: Array<any> = [];
+    let character = "  |\n";
+    let char: string = "";
+    arr = character.split("")
+    for (let i = 1; i < size; i++) {
+      arr.splice(1, 0, " ")
+    }
+    character = arr.join("");
+    char = character;
+    for (let j = 1; j < size; j++) {
+      character = character + char;
+    }
+    return character
+  }
+
+  assignLeftBar(size: number): string {
+    let arr: Array<any> = [];
+    let character = "|";
+    let char: string = "";
+    if (size > 1) {
+      arr = character.split("");
+      arr.push("\n");
+      character = arr.join("");
+      char = character;
+      for (let j = 1; j < size; j++) {
+        character = character + char;
+      }
+    }
+    return character
+  }
+
+  assignBothBars(size: number): string {
+    let arr: Array<any> = [];
+    let character = "| |\n";
+    let char: string = "";
+    if (size > 1) {
+      arr = character.split("")
+      for (let i = 1; i < size; i++) {
+        arr.splice(1, 0, " ")
+      }
+      character = arr.join("");
+      char = character;
+      for (let j = 1; j < size; j++) {
+        character = character + char;
+      }
+    }
+    return character
+  }
+
+  assignSpace(size: number): string {
+    return "\n"
+  }
+
+
+
+}
